657969df4ddafdfd0e97f1d4494a72172154bec3
linus <linus.f@protonmail.ch>
2024-03-12 22:37:39 +0100

    Remade api client to be more generic. Added some graceful error handling

--- src/git_host_client.rs
+++ src/git_host_client.rs
@@ -1,7 +1,9 @@
 use std::env;
 use reqwest::Method;
 use reqwest::*;
-use serde_json;
+use core::fmt;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+
 
 pub struct GitHostClient {
     url: String,
@@ -17,35 +19,51 @@ impl GitHostClient {
 
         match (user_agent, url, token) {
             (Ok(user_agent), Ok(url), Ok(token)) => GitHostClient {url, token, user: Some(String::from(user_agent))},
-            _ => {panic!(".env problems");}
+            _ => {panic!("USER_AGENT, GIT_HOST_URL and GIT_HOST_TOKEN must be defined in environment.");}
         }
         
     }
 
-    pub async fn request(&self, method: Method, endpoint: &str, _data: Option<String>) -> Result<serde_json::Value> {
+    pub async fn request<T>(&self, method: Method, endpoint: &str, _data: Option<String>) -> Result<T>
+    where T: DeserializeOwned + Serialize + fmt::Debug
+    {
         let client = reqwest::Client::new();
-        let mut request_url = self.url.clone();
-
-        request_url.push_str(endpoint);
+        let request_url = format!("{}{}", &self.url, endpoint);
+        let token = format!("{}{}", "Bearer ", &self.token);
 
         let body = match method {
             Method::GET => {
                 client
                     .get(request_url)
                     .header("User-Agent", self.user.as_ref().unwrap())
+                    .header::<&str, &str>("Authorization", &token)
                     .send().await
             },
             _ => { todo!() }
         };
 
-        let json: serde_json::Value = match body {
+        let json: Result<T> = match body {
             Ok(res) => {
-                res.json().await.unwrap()
+                res.json().await
             },
-            _ => { serde_json::Value::Null }
+            Err(err) => { Err(err) }
         };
 
-        // println!("{:#?}", json[0].get("full_name"));
-        Ok(json)
+        match json {
+            Ok(value) => {
+                println!("{:#?}", value);
+                Ok(value)
+            },
+            Err(err) => {
+                println!("Error decoding JSON: {:?}", err);
+                Err(err)
+            }
+        }
     }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GithubRepoResponse {
+    pub full_name: String,
+    pub ssh_url: String
 }
